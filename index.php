07111bdf7412bcd50f1183c8f25faaea8e4eb750
Anne-Laure DAY <alday@hotmail.fr>
2018-06-28 14:18:17 +0200

    ajout background

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GIT test</title>
+    <style>
+    body{
+        background : yellow;
+    }
+    h1{
+        position : absolute;
+        left : 45%;
+        top : 45%;
+        color: blue;
+    }
+    </style>
+</head>
+<body>    
+</body>
+</html>
